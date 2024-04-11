0144980c37f4ef7798e7cea29f9d9f56ebad3ed6
Kaitlin <nicholas.lucioforlife@yahoo.com>
2024-04-11 01:23:07 -0700

    Simulink running, model functioning, close to correct poles placed

--- main.m
+++ main.m
@@ -5,25 +5,30 @@ clear
 
 L = 0.5;
 g = 9.8;
+alpha = 0.5;
+pmod = 10;
 
 % Pole calculations
 
-zetaCon = 2;
-wCon = 3;
+zetaCon = 1.01;
+wCon = 1.98;
 conPolynom = [1 2*zetaCon*wCon wCon^2];
 conPoles = roots(conPolynom);
-conPoles = [conPoles', 15*max(conPoles), 20*max(conPoles)];
-sys = tf(wCon^2, conPolynom);
-step(sys)
+r_con_p = real(min(conPoles));
+i_con_p = imag(min(conPoles));
+conPoles = [conPoles', pmod*r_con_p, (pmod+1)*r_con_p];
 
 zetaObs = 1;
-wObs = 13.2;
+wObs = 8;
 obsPolynom = [1 2*zetaObs*wObs wObs^2];
 obsPoles = roots(obsPolynom);
 % sys = tf(wObs^2, obsPolynom);
 % step(sys)
 
-obsPoles = [obsPoles', 15*max(obsPoles), 20*max(obsPoles)];
+min_obs_pole = max(obsPoles);
+r_obs_p = real(min_obs_pole);
+i_obs_p = imag(min_obs_pole);
+obsPoles = [obsPoles', pmod*(r_obs_p+i_obs_p),pmod*(r_obs_p-i_obs_p)];
 
 
 % Define A, B, C, and D
@@ -42,7 +47,25 @@ C = [
     0, 0, 1, 0
     ];
 
-D = 0;
+D = [
+    0;
+    0
+    ];
 
 [Mc, invMc] = testControllability(A, B);
 [Mo, invMo] = testObservability(A, C);
+
+% Calculate Observer and Controller using poles
+
+K = place(A, B, conPoles);
+Lo = place(A', C', obsPoles)';
+
+% Calculate Ki for the system
+
+Ki = 15*r_con_p;
+
+% Simulate Simulink model
+
+sim_run = sim("plant.mdl");
+plot(sim_run.yout)
+
