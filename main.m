6b3ae7142e02b5441c8a3f958b815c69f79edd9e
Kaitlin <nicholas.lucioforlife@yahoo.com>
2024-04-17 12:33:38 -0700

    Changes to main.m to make it work, LaTeX updates

--- main.m
+++ main.m
@@ -1,24 +1,27 @@
 % Written by Pierce Gruber and Kaitlin Lucio
 clear
+clf
 
 % Variable definitions
 
 L = 0.5;
 g = 9.8;
 alpha = 0.5;
-pmod = 7;
+pmod = 10;
+dist = 0;
+testmode = true;
 
 % Pole calculations
 
-zetaCon = 1.01;
-wCon = 2.92;
+zetaCon = 1.2;
+wCon = 0.5;
 conPolynom = [1 2*zetaCon*wCon wCon^2];
 conPoles = roots(conPolynom);
 r_con_p = real(min(conPoles));
 i_con_p = imag(min(conPoles));
-conPoles = [conPoles', pmod*r_con_p, (pmod+0.1)*r_con_p];
+conPoles = [conPoles', pmod*r_con_p, (pmod+1)*r_con_p];
 
-zetaObs = 1;
+zetaObs = 1.5;
 wObs = 1;
 obsPolynom = [1 2*zetaObs*wObs wObs^2];
 obsPoles = roots(obsPolynom);
@@ -57,15 +60,59 @@ D = [
 
 % Calculate Observer and Controller using poles
 
-K = place(A, B, conPoles);
+K_old = place(A, B, conPoles);
 Lo = place(A', C', obsPoles)';
 
 % Calculate Ki for the system
 
-Ki = pmod*r_con_p;
+Ki_old = pmod*r_con_p;
+
+
+% Calculate Ki_fb and K_fb using integral action state matrices
+
+conPoles = [conPoles(end)+0.1, conPoles];
+
+Aaf = [
+    0, -C(2,:); 
+    zeros(4,1), A
+    ];
+Baf = [
+    -D(2); B
+];
+
+Ktot = place(Aaf, Baf, conPoles);
+Ki = Ktot(1)
+K = Ktot(2:end)
 
 % Simulate Simulink model
-
-sim_run = sim("plant.mdl");
-plot(sim_run.yout)
-
+if testmode
+    sim_run = sim("plant.mdl");
+    plot(sim_run.yout)
+else
+    distvec = 0:0.1:0.5;
+    
+    i = 1;
+    
+    figurefolder = "figures/";
+    figprefix = "sim_displacement_";
+    
+    for dist=distvec
+        figure(i)
+        sim_run = sim("plant.mdl");
+        s = sim_run.yout{2}.Values.Data;
+        theta = sim_run.yout{3}.Values.Data;
+        a = sim_run.yout{4}.Values.Data;
+        time = sim_run.tout;
+        hold on
+        plot(time, s)
+        plot(time, theta)
+        plot(time, a)
+        xlabel("Time (s)")
+        ylabel("Magnitude of output")
+        legend("Displacement [m]", "Angle of Pendulum [rad]", "Acceleration [$m/s^2$]")
+        title("System response to disturbance of "+num2str(dist)+" in rad/s")
+        saveas(i,figurefolder+num2str(dist)+".png")
+        close(i)
+        i = i + 1;
+    end
+end
