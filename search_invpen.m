4cdfc73161c558d90e4d9b300d30804802544bee
omenmis <nicholas.lucioforlife@yahoo.com>
2024-04-17 17:22:51 -0700

    Gridsearch variablized

--- search_invpen.m
+++ search_invpen.m
@@ -2,9 +2,11 @@
     Wrapper file to find good poles for an inverted pendulum
 %}
 
-zetarange = 3.4:0.01:3.7;
-wrange = 0.18:0.005:0.23;
+zetarange = 3.55:0.01:3.8;
+wrange = 0.183:0.001:0.21;
 disp = 0.5;
+t_settle_thres = 15;
+os_thres = 0.01;
 
 outcsv = "figures/invpen_vals_disp"+num2str(disp)+".csv";
 file = fopen(outcsv,"w");
@@ -15,7 +17,7 @@ k = 0;
 for zetaCon = zetarange
     for wCon = wrange
         [os, t_settle, sys_data] = sim_invpendulum(disp,zetaCon,wCon);
-        if (os < 0.01) && (t_settle < 25)
+        if (os < os_thres) && (t_settle < t_settle_thres)
             fprintf(file, fspec, k, zetaCon, wCon, t_settle);
             k = k + 1;
         end
